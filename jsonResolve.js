997571c094466fc7057a3b7b420791bde000191c
shenchanghai <shenchanghai@geekplus.com.cn>
2020-04-08 16:07:17 +0800

    node生成json文件

--- jsonResolve.js
+++ jsonResolve.js
@@ -1,5 +1,7 @@
 var fs = require("fs");
 let data='';
+let data1={};
+let data2={};
 var readerStream = fs.createReadStream('./lang_zh.json');
 readerStream.setEncoding('UTF8');
 readerStream.on('data', function(chunk) {
@@ -7,19 +9,39 @@ readerStream.on('data', function(chunk) {
 });
 
 readerStream.on('end',function(){
-  data = JSON.parse(data);
-  for(let i in data){
+  let obj = {};
+  data1 = JSON.parse(data);
+  //data2 = JSON.parse(data);
+  let arr = Object.values(data1);
+
+  let arr1 = [];
+  let arr2 = [];
+  arr.forEach((item) => {
+    if(arr1.indexOf(item) > -1 && arr2.indexOf(item) === -1){
+      arr2.push(item);
+    }else {
+      arr1.push(item);
+    }
+  });
+
+  for(let i in data1){
+    if(arr2.indexOf(data1[i]) > -1){
+      obj[i] = data1[i];
+    }
+  }
+  /*for(let i in data){
     if(i.indexOf('fed.') === -1){
       delete data[i];
     }
-  }
-  data = JSON.stringify(data);
-  var writerStream = fs.createWriteStream('./lang_zh.json');
+  }*/
+  console.log(obj)
+  fs.writeFileSync('./repetition.json',JSON.stringify(obj));
+  /*var writerStream = fs.createWriteStream('./lang_zh.json');
   writerStream.write(data,'UTF8');
   writerStream.end();
   writerStream.on('finish', function() {
     console.log("写入完成。");
-  });
+  });*/
 
 });
 
