0837c89d45430a318def1e9f0a724bf69440b0ed
shenchanghai <shenchanghai@geekplus.com.cn>
2020-04-08 18:07:29 +0800

    新增异步处理

---
+++ js/asy.js
@@ -0,0 +1,16 @@
+// 模拟其他语言中的 sleep，实际上可以是任何异步操作
+const sleep = (timeountMS) => new Promise((resolve) => {
+  setTimeout(resolve, timeountMS);
+});
+
+(async () => {  // 声明即执行的 async 函数表达式
+  for (var i = 0; i < 5; i++) {
+    if (i > 0) {
+      await sleep(1000);
+    }
+    console.log(new Date, i);
+  }
+
+  await sleep(1000);
+  console.log(new Date, i,100);
+})();
