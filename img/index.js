6d16d798ceff9426a0b5ad67f55368e3440d9144
shenchanghai <shenchanghai@geekplus.com.cn>
2020-04-13 11:28:04 +0800

    图片处理 进一步完善

--- img/index.js
+++ img/index.js
@@ -19,23 +19,24 @@ temp_qrcode.pipe(fs.createWriteStream('i_love_qr.png'));*/
   console.error(err);
 });*/
 
-let p = Promise.all([Jimp.read('i_love_qr.png'), Jimp.read('test.jpeg')]);
+let p = Promise.all([Jimp.read('dtest.png'), Jimp.read('stest.png')]);
 
 p.then(function (data) {
   let img1 = data[1];
   let img0 = data[0];
-  img0.resize(100, 100);
+  // img0.resize(100, 100);
   let source = img0.bitmap.data;
   let source1 = img1.bitmap.data;
-  img1.resize(16, 16);
+  //img1.resize(16, 16);
   let width = img1.bitmap.width;
   let height = img1.bitmap.height;
 
-   new Jimp(100, 100, function (err, saveimage) {
-    let buffer = source;
-     /*source1.forEach((item,index) => {
+   new Jimp(11, 6, function (err, saveimage) {
+    let buffer = source1;
+     source.forEach((item,index) => {
        buffer[index] = item;
-     })*/
+     })
+
     /*for (var x = 0; x < width; x++) {
       for (var y = 0; y < height; y++) {
         let offset = ( width * x) +y* 4 // RGBA = 4 bytes
@@ -45,16 +46,16 @@ p.then(function (data) {
         buffer[offset + 3] = source1[offset+3]  // Alpha
       }
     }*/
-    console.log('saveimage',saveimage.composite)
     saveimage.bitmap.data=buffer;
+     /*//图片合并
      saveimage.composite(img1, 0, 0, {
        mode: Jimp.BLEND_SOURCE_OVER,
        opacitySource: 0.5,
        opacityDest: 0.1
-     });
-    setTimeout(() => {
-      saveimage.write('image.jpeg')
-    },1000)
+     });*/
+     //截取一个图片后再合并
+    // saveimage.blit(img1, 4, 1,1,0,1,1);
+     saveimage.write('image2.jpeg')
   });
 },function (err) {
   console.log(err);
